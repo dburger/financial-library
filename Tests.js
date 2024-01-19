20df2d312162f0b665ccf6a910a17105602ce035
David J. Burger <davidburger@gmail.com>
2024-01-19 11:51:21 -0700

    Change tests to use predicate function

--- Tests.js
+++ Tests.js
@@ -1,10 +1,10 @@
 function testFinancialLibrary() {
   const failures = [];
 
-  assert(failures, 27.08, finviz, "GOOG", 0, 3);
+  assert(failures, (val) => val > 15.0 && val < 40.0, finviz, "GOOG", 0, 3);
   assert(failures, 0.0003, yahooExpenseRatio, "VTI");
-  assert(failures, 6709857, yahooVolume, "GOOG");
-  assert(failures, 1.0, yahooAverageVolume, "GOOG");
+  assert(failures, (val) => val > 0, yahooVolume, "GOOG");
+  assert(failures, (val) => val > 10000000 && val < 50000000, yahooAverageVolume, "GOOG");
 
   if (failures.length > 0) {
     console.error(failures.join("\n"));
@@ -15,7 +15,11 @@ function testFinancialLibrary() {
 function assert(failures, expected, func, ...args) {
   try {
     const actual = func(...args);
-    if (actual !== expected) {
+    if (expected instanceof Function) {
+      if (!expected(actual)) {
+        failures.push(`${func.name} expected predicate failed.`);
+      }
+    } else if (actual !== expected) {
       failures.push(`${func.name} expected ${expected}, actual ${actual}.`);
     }
   } catch (exc) {
